53cf0f793c79b887ee4aec8067ec68eebd82feea
BraisC <braiscao.dev@gmail.com>
2022-11-09 11:14:23 +0100

    Added another extra

---
+++ advanced-react-patterns/src/final/02.extra-2.js
@@ -0,0 +1,53 @@
+// Compound Components
+// 💯 Support non-toggle children
+// http://localhost:3000/isolated/final/02.extra-2.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function Toggle({children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+  return React.Children.map(children, child => {
+    if (allowedTypes.includes(child.type)) {
+      const newChild = React.cloneElement(child, {on, toggle})
+      return newChild
+    }
+
+    return child
+  })
+}
+
+function ToggleOn({on, children}) {
+  return on ? children : null
+}
+
+function ToggleOff({on, children}) {
+  return on ? null : children
+}
+
+function ToggleButton({on, toggle, ...props}) {
+  return <Switch on={on} onClick={toggle} {...props} />
+}
+
+function MyToggleButton({on, toggle}) {
+  return on ? 'The toggle is on yooo' : 'The toggle is off so...'
+}
+
+const allowedTypes = [ToggleOn, ToggleOff, ToggleButton]
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
+        <ToggleButton />
+        <MyToggleButton />
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
